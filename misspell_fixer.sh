b5ec1f7af8d6d0e1bc51695fe09b94c8f0618156
Veres Lajos <vlajos@gmail.com>
2014-12-19 09:57:09 +0000

    make misspell_fixer and its test kcov friendly

--- misspell_fixer.sh
+++ misspell_fixer.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
-set -f
-
-export opt_debug=0
-export opt_verbose=0
-export opt_show_diff=0
-export opt_fast_mode=0
-export opt_real_run=0
-export opt_backup=1
-export opt_ignore_scm_dirs=1
-export opt_parallelism=0
-
-rules_safe0=$(echo $0|sed 's/\.sh$/_safe.0.sed/')
-rules_safe1=$(echo $0|sed 's/\.sh$/_safe.1.sed/')
-rules_safe2=$(echo $0|sed 's/\.sh$/_safe.2.sed/')
-rules_not_so_safe=$(echo $0|sed 's/\.sh$/_not_so_safe.sed/')
-rules_gb_to_us=$(echo $0|sed 's/\.sh$/_gb_to_us.sed/')
-export cmd_part_rules="-f $rules_safe0"
-
-export cmd_part_ignore=" ! -wholename *.git* ! -wholename *.svn* "
-
-export opt_name_filter=''
-
 function warning {
 	echo "misspell_fixer: $@">&2
 }
+
 function verbose {
 	if [[ $opt_verbose = 1 ]]
 	then
@@ -32,114 +11,157 @@ function verbose {
 	fi
 }
 
-while getopts ":dvrfsinRVughN:P:" opt; do
-	case $opt in
-		d)
-			warning "-d Enabling debug mode."
-			opt_debug=1
-		;;
-		v)
-			warning "-v Enabling verbose mode."
-			opt_verbose=1
-		;;
-		r)
-			warning "-r Enabling real run. Overwrite original files!"
-			opt_real_run=1
-		;;
-		f)
-			warning "-f Enabling fast mode."
-			opt_fast_mode=1
-		;;
-		s)
-			warning "-s Enabling showing of diffs."
-			opt_show_diff=1
-		;;
-		i)
-			warning "-i Disable scm dir ignoring."
-			opt_ignore_scm_dirs=1
-			cmd_part_ignore=''
-		;;
-		n)
-			warning "-n Disabling backups."
-			opt_backup=0
-		;;
-		u)
-			warning "-u Enabling unsafe rules."
-			cmd_part_rules="$cmd_part_rules -f $rules_not_so_safe"
-		;;
-		R)
-			warning "-R Enabling rare rules."
-			cmd_part_rules="$cmd_part_rules -f $rules_safe1"
-		;;
-		V)
-			warning "-V Enabling very-rare rules."
-			cmd_part_rules="$cmd_part_rules -f $rules_safe2"
-		;;
-		g)
-			warning "-g Enabling GB to US rules."
-			cmd_part_rules="$cmd_part_rules -f $rules_gb_to_us"
-		;;
-		N)
-			warning "-N Enabling name filter: $OPTARG"
-			if [ -n "$opt_name_filter" ]; then
-				opt_name_filter="$opt_name_filter -or -name $OPTARG"
-			else
-				opt_name_filter="-name $OPTARG"
-			fi
-		;;
-		P)
-			warning "-P Enabling parallelism: $OPTARG"
-			opt_parallelism=$OPTARG
-		;;
-		h)
-			cat $(dirname $0)/README.md
-			exit
-		;;
-		\?)
-			warning "Invalid option: -$OPTARG"
-			exit
-		;;
-		:)
-			warning "Option -$OPTARG requires an argument."
-			exit
-		;;
-	esac
-done
+function init_variables {
+	set -f
 
-if [ -z "$opt_name_filter" ]; then
-	opt_name_filter='-true'
-fi
+	export opt_debug=0
+	export opt_verbose=0
+	export opt_show_diff=0
+	export opt_fast_mode=0
+	export opt_real_run=0
+	export opt_backup=1
+	export opt_ignore_scm_dirs=1
+	export opt_parallelism=0
 
-shift $((OPTIND-1))
+	rules_safe0=$(echo $1|sed 's/\.sh$/_safe.0.sed/')
+	rules_safe1=$(echo $1|sed 's/\.sh$/_safe.1.sed/')
+	rules_safe2=$(echo $1|sed 's/\.sh$/_safe.2.sed/')
+	rules_not_so_safe=$(echo $1|sed 's/\.sh$/_not_so_safe.sed/')
+	rules_gb_to_us=$(echo $1|sed 's/\.sh$/_gb_to_us.sed/')
+	export cmd_part_rules="-f $rules_safe0"
 
-if [[ "$@" = "" ]]
-then
-	warning "Not enought arguments. (target directory not found) => Exiting."
-	exit
-fi
-warning "Target directories: $@"
+	export cmd_part_ignore=" ! -wholename *.git* ! -wholename *.svn* "
 
-if [[ $opt_fast_mode = 1 ]]
-then
+	export opt_name_filter=''
+
+	export directories
+}
+
+function parse_basic_options {
+	local OPTIND
+	while getopts ":dvrfsinRVughN:P:" opt; do
+		case $opt in
+			d)
+				warning "-d Enabling debug mode."
+				opt_debug=1
+			;;
+			v)
+				warning "-v Enabling verbose mode."
+				opt_verbose=1
+			;;
+			r)
+				warning "-r Enabling real run. Overwrite original files!"
+				opt_real_run=1
+			;;
+			f)
+				warning "-f Enabling fast mode."
+				opt_fast_mode=1
+			;;
+			s)
+				warning "-s Enabling showing of diffs."
+				opt_show_diff=1
+			;;
+			i)
+				warning "-i Disable scm dir ignoring."
+				opt_ignore_scm_dirs=1
+				cmd_part_ignore=''
+			;;
+			n)
+				warning "-n Disabling backups."
+				opt_backup=0
+			;;
+			u)
+				warning "-u Enabling unsafe rules."
+				cmd_part_rules="$cmd_part_rules -f $rules_not_so_safe"
+			;;
+			R)
+				warning "-R Enabling rare rules."
+				cmd_part_rules="$cmd_part_rules -f $rules_safe1"
+			;;
+			V)
+				warning "-V Enabling very-rare rules."
+				cmd_part_rules="$cmd_part_rules -f $rules_safe2"
+			;;
+			g)
+				warning "-g Enabling GB to US rules."
+				cmd_part_rules="$cmd_part_rules -f $rules_gb_to_us"
+			;;
+			N)
+				warning "-N Enabling name filter: $OPTARG"
+				if [ -n "$opt_name_filter" ]; then
+					opt_name_filter="$opt_name_filter -or -name $OPTARG"
+				else
+					opt_name_filter="-name $OPTARG"
+				fi
+			;;
+			P)
+				warning "-P Enabling parallelism: $OPTARG"
+				opt_parallelism=$OPTARG
+			;;
+			h)
+				cat $(dirname $0)/README.md
+				return 1
+			;;
+			\?)
+				warning "Invalid option: -$OPTARG"
+				return 1
+			;;
+			:)
+				warning "Option -$OPTARG requires an argument."
+				return 1
+			;;
+		esac
+	done
+	
+	if [ -z "$opt_name_filter" ]; then
+		opt_name_filter='-true'
+	fi
+	
+	shift $((OPTIND-1))
+	
+	if [[ "$@" = "" ]]
+	then
+		warning "Not enought arguments. (target directory not found) => Exiting."
+		return 1
+	fi
+
+	directories="$@"
+	warning "Target directories: $directories"
+
+	return 0
+}
+
+function main_work {
+	if [[ $opt_fast_mode = 1 ]]
+	then
+		main_work_fast "$1"
+		return $?
+	else
+		main_work_normal "$1"
+		return $?
+	fi
+}
+
+function main_work_fast {
 	if [[ $opt_real_run = 0 ]]
 	then
 		warning "Fast mode works only with real run. Real run is not switched on. => Exiting."
-		exit
+		return 1
 	fi
 	if [[ $opt_backup = 1 ]]
 	then
 		warning "Fast mode cannot make backups. Backups are enabled. => Exiting."
-		exit
+		return 1
 	fi
 	if [[ $opt_show_diff = 1 ]]
 	then
 		warning "Fast mode cannot show diffs. Showing diffs is turned on. => Exiting."
-		exit
+		return 1
 	fi
 	if [[ $opt_verbose = 1 ]]
 	then
 		warning "Fast mode cannot be verbose. Verbose mode is turned on. => Exiting."
-		exit
+		return 1
 	fi
 
 	warning "Starting script"
@@ -149,14 +171,13 @@ then
 	fi
 	if [[ $opt_parallelism = 0 ]]
 	then
-		find "$@" -type f $cmd_part_ignore -and \( $opt_name_filter \) -exec sed -i -b $cmd_part_rules {} +
+		find "$directories" -type f $cmd_part_ignore -and \( $opt_name_filter \) -exec sed -i -b $cmd_part_rules {} +
 	else
-		find "$@" -type f $cmd_part_ignore -and \( $opt_name_filter \) -print0|xargs -0 -P $opt_parallelism -n 100 sed -i -b $cmd_part_rules
+		find "$directories" -type f $cmd_part_ignore -and \( $opt_name_filter \) -print0|xargs -0 -P $opt_parallelism -n 100 sed -i -b $cmd_part_rules
 	fi
-	set +x
 	warning "Done."
-	exit;
-fi
+	return 0
+}
 
 function loop_core {
 	if [[ $opt_debug = 1 ]]
@@ -194,14 +215,42 @@ function loop_core {
 }
 export -f loop_core verbose warning
 
-if [[ $opt_debug = 1 ]]
+function main_work_normal {
+	if [[ $opt_debug = 1 ]]
+	then
+		set -x
+	fi
+	warning "Starting script"
+	find "$directories"\
+		-type f\
+		$cmd_part_ignore \
+		-and \( $opt_name_filter \) \
+		-exec bash -c 'loop_core "$0"' {} \;
+	warning "Done."
+	return 0
+}
+
+if [[ $UNDERSHUNIT = 1 ]]
 then
-	set -x
+	main_executable=./misspell_fixer.sh
+else
+	main_executable=$0
 fi
-warning "Starting script"
-find "$@"\
-	-type f\
-	$cmd_part_ignore \
-	-and \( $opt_name_filter \) \
-	-exec bash -c 'loop_core "$0"' {} \;
-warning "Done."
+
+init_variables $main_executable
+
+parse_basic_options "$@"
+retval=$?
+
+if [[ $retval == 0 ]]
+then
+	main_work "$directories"
+	retval=$?
+fi
+
+if [[ $UNDERSHUNIT = 1 ]]
+then
+	return $retval
+else
+	exit $retval
+fi
--- test/tests.sh
+++ test/tests.sh
@@ -1,68 +1,68 @@
-#! /bin/sh
+#! /bin/bash
 
 export TEMP=/tmp/misspell_fixer_test/$$
+export RUN=". misspell_fixer.sh"
+export UNDERSHUNIT=1
 
 oneTimeSetUp(){
-    mkdir -p $TEMP $TEMP/work/ $TEMP/expected/
+	mkdir -p $TEMP $TEMP/work/ $TEMP/expected/
 }
 
 oneTimeTearDown(){
-   rm -rf $TEMP
+	rm -rf $TEMP
 }
 
 setUp(){
-    cp -a test/stubs/* $TEMP/work/
-    cp -a test/stubs/* $TEMP/expected/
-}
-
-testNoArg(){
-    assertEquals "misspell_fixer: Not enought arguments. (target directory not found) => Exiting." "$(./misspell_fixer.sh 2>&1)"
+	set +f
+	cp -a test/stubs/* $TEMP/work/
+	cp -a test/stubs/* $TEMP/expected/
+	set -f
 }
 
 runAndCompare(){
-    ./misspell_fixer.sh $1 $TEMP/work
-    while [ "$2" != "" ]
-    do
-        cp -a test/expecteds/$2.txt $TEMP/expected/
-        shift
-    done
-    diff -ruwb $TEMP/expected/ $TEMP/work/
-    assertTrue 'Expected output differs.' $?
+	$RUN $1 $TEMP/work
+	assertTrue $?
+	while [ "$2" != "" ]
+	do
+		cp -a test/expecteds/$2.txt $TEMP/expected/
+		shift
+	done
+	diff -ruwb $TEMP/expected/ $TEMP/work/
+	assertTrue 'Expected output differs.' $?
+}
+
+testNoArg(){
+	TMP=$($RUN 2>&1)
+	assertFalse $?
+	echo $TMP|grep -q "misspell_fixer: Not enought arguments. (target directory not found) => Exiting."
+	assertTrue 'No argument handling problem.' $?
 }
 
 testOnlyDir(){
-    runAndCompare '';
+	runAndCompare '';
 }
 
-testReal0NoBackup(){
-    runAndCompare '-rn' '0'
-}
-
-testFast0(){
-    runAndCompare '-frn' '0'
-}
-testFastR(){
-    runAndCompare '-frnR' '0' 'R'
-}
-testFastV(){
-    runAndCompare '-frnV' '0' 'V'
-}
-testFastu(){
-    runAndCompare '-frnu' '0' 'u'
-}
-testFastg(){
-    runAndCompare '-frng' '0' 'g'
-}
-testFastRVug(){
-    runAndCompare '-frnRVug' '0' 'R' 'V' 'u' 'g'
-}
 testSpaceInFileName(){
-    mv $TEMP/work/0.txt "$TEMP/work/0 0.txt"
-    rm $TEMP/expected/0.txt
-    cp -a test/expecteds/0.txt "$TEMP/expected/0 0.txt"
-    ./misspell_fixer.sh -rn $TEMP/work
-    diff -ruwb $TEMP/expected/ $TEMP/work/
-    assertTrue 'Expected output differs.' $?
+	mv $TEMP/work/0.txt "$TEMP/work/0 0.txt"
+	rm $TEMP/expected/0.txt
+	cp -a test/expecteds/0.txt "$TEMP/expected/0 0.txt"
+	$RUN -rn $TEMP/work
+	diff -ruwb $TEMP/expected/ $TEMP/work/
+	assertTrue 'Expected output differs.' $?
+}
+
+suite(){
+    suite_addTest testNoArg
+    suite_addTest testOnlyDir
+    suite_addTest testSpaceInFileName
+    for i in '' R V u g 'R V u g'
+    do
+        allarg=$(echo $i|sed 's/ //g')
+        eval "testMainNormal$allarg(){ runAndCompare -rn$allarg 0 $i; }"
+        suite_addTest testMainNormal$allarg
+        eval "testMainFast$allarg(){ runAndCompare -frn$allarg 0 $i; }"
+        suite_addTest testMainFast$allarg
+    done
 }
 
 
