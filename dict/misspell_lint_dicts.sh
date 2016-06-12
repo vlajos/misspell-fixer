33818405620bfeccc204582227c88a2ec83aa1ef
klemens <ka7@github.com>
2016-06-12 15:14:41 +0200

    fix some possible-problematic shell commands

--- dict/misspell_lint_dicts.sh
+++ dict/misspell_lint_dicts.sh
@@ -2,11 +2,11 @@
 
 function remove_duplications {
 	export actual=$1
-	sort -u /tmp/already_processed.dict >/tmp/already_processed.dict.su
-	sort -u misspell_fixer_$actual.dict >misspell_fixer_$actual.dict.su
-	comm -23 misspell_fixer_$actual.dict.su /tmp/already_processed.dict.su >misspell_fixer_$actual.dict
+	sort -u /tmp/already_processed.dict > /tmp/already_processed.dict.su
+	sort -u "misspell_fixer_$actual.dict" > "misspell_fixer_$actual.dict.su"
+	comm -23 "misspell_fixer_$actual.dict.su" /tmp/already_processed.dict.su > "misspell_fixer_$actual.dict"
 	mv /tmp/already_processed.dict.su /tmp/already_processed.dict
-	rm misspell_fixer_$actual.dict.su
+	rm "misspell_fixer_$actual.dict.su"
 }
 
 sort -u misspell_fixer_safe.0.dict >/tmp/already_processed.dict
