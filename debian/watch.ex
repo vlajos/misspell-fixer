0c63f6ed032180319cffd816c8653f22ac969676
Lajos Veres <vlajos@gmail.com>
2018-03-07 23:10:28 +0000

    Debian packaging - first version.

---
+++ debian/watch.ex
@@ -0,0 +1,19 @@
+# Example watch control file for uscan
+# Rename this file to "watch" and then you can run the "uscan" command
+# to check for upstream updates and more.
+# See uscan(1) for format
+
+# Compulsory line, this is a version 4 file
+version=4
+
+# PGP signature mangle, so foo.tar.gz has foo.tar.gz.sig
+#opts="pgpsigurlmangle=s%$%.sig%"
+
+# GitHub hosted projects
+#opts="filenamemangle=s%(?:.*?)?v?(\d[\d.]*)\.tar\.gz%<project>-$1.tar.gz%" \
+#   https://github.com/<user>/misspell-fixer/tags \
+#   (?:.*?/)?v?(\d[\d.]*)\.tar\.gz debian uupdate
+
+# Direct Git
+# opts="mode=git" http://git.example.com/misspell-fixer.git \
+#   refs/tags/v([\d\.]+) debian uupdate
