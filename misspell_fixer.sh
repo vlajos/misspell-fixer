b1f53d7ea318283e5f048ccf7d90c7fb6eae3aec
Veres Lajos <vlajos@gmail.com>
2018-09-16 22:04:30 +0100

    Split complex expression to more manageable ones

--- misspell_fixer.sh
+++ misspell_fixer.sh
@@ -3,4 +3,6 @@
 # Compatibility wrapper for the old name.
 
 echo "misspell-fixer has been renamed. Please use the new name: misspell-fixer"
-$(dirname "$BASH_SOURCE[0]")/misspell-fixer "$@"
+current_file="$BASH_SOURCE[0]"
+dir_name=$(dirname "$current_file")
+"$dir_name/misspell-fixer" "$@"
