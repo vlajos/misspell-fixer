6be12af79984e1cb0857f25590625b9b711546dd
Veres Lajos <vlajos@gmail.com>
2019-09-30 23:32:12 +0100

    Shellcheck explicit shell type declaration

--- test/coverage_wrapper.sh
+++ test/coverage_wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 d=$(dirname "${BASH_SOURCE[0]}")
-# shellcheck source=lib/initialisation_functions.sh
+# shellcheck source=lib/initialisation_functions.sh shell=bash
 source "$d/../lib/initialisation_functions.sh"
-# shellcheck source=lib/main_functions.sh
+# shellcheck source=lib/main_functions.sh shell=bash
 source "$d/../lib/main_functions.sh"
 
 eval "$3" >>"$TEST_OUTPUT" 2>&1
