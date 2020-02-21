30b88e66f4cf9598dc111349fddb800e426683bd
Veres Lajos <vlajos@gmail.com>
2020-02-21 19:16:12 +0000

    Standardize timezone sign in test output diffs

--- test/tests.sh
+++ test/tests.sh
@@ -41,7 +41,7 @@ runAndCompareOutput(){
     diff -ruwb $TEMP/expected/ $TEMP/work/
     assertTrue 'Expected output differs.' $?
 
-    sed 's/[0-9]\+/X/g' "$TEST_OUTPUT" |\
+    sed -e 's/[0-9]\+/X/g' -e 's/X -X$/X +X/g' "$TEST_OUTPUT" |\
     grep -v -e kcov -e 'grep -vh bin/sed' -e "Your grep version is" \
         >"$TEST_OUTPUT.standard"
     if [[ "$3" = "1" ]]; then
